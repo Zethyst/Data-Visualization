f0ca19677386b20b5744c18cdee81bdcf3d3812c
Ekansh <zethyst@protonmail.com>
2024-05-15 15:11:34 +0530

    Made changes necessary for deployment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className="relative bg-[#F1F1F1] h-[300vh]">
+    <div className="relative bg-[#F1F1F1] h-[280vh]">
       <Navbar title={title}/>
       <Sidebar setTitle={setTitle}/>
       <Routes>
